000a07f5159e59c5239347ed1263bec6fa957498
ashirwadsingh857-beep <ashirwadsingh857@gmail.com>
2026-02-22 20:46:03 +0530

    Fixed ENETUNREACH by forcing IPv4 for Gmail

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,8 @@ const transporter = nodemailer.createTransport({
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
-    }
+    },
+    family: 4
 });
 
 // Load .env from the current directory (/backend)
